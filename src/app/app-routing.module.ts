38431501c49a7145793ad67b5666ce1333fc3de8
localhostvn <nguynhuan76@gmail.com>
2021-03-21 00:18:01 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './page/detail/detail.component';
 import { HomeComponent } from './page/home/home.component';
+import { PayComponent } from './page/pay/pay.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: 'pay', component: PayComponent }
 ];
 
 @NgModule({
