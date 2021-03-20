152524adc71bec36b1d5525d39a9e2c2bd58eecc
localhostvn <nguynhuan76@gmail.com>
2021-03-20 16:19:38 +0700

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+import { MenuComponent } from './block/menu/menu.component';
+import { BookComponent } from './block/book/book.component';
+import { ShapeComponent } from './block/shape/shape.component';
+import { FooterComponent } from './block/footer/footer.component';
+import { PageComponent } from './page/page.component';
+import { HomeComponent } from './page/home/home.component';
+import { PayComponent } from './page/pay/pay.component';
+import { DetailComponent } from './page/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockComponent,
+    MenuComponent,
+    BookComponent,
+    ShapeComponent,
+    FooterComponent,
+    PageComponent,
+    HomeComponent,
+    PayComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
